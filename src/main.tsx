978c0e403a0065e4e65bf5b03f312827c24ff390
fedirdziuba <candelight1@gmail.com>
2022-12-07 11:12:05 -0800

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+const bootstrap = () => {
+  const root = document.getElementById("root");
+  ReactDOM.render(<React.StrictMode></React.StrictMode>, root);
+};
+
+bootstrap();
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  mode: "jit",
+  purge: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {},
+  variants: {},
+  plugins: [],
+};
